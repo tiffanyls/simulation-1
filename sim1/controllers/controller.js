6d980264b5eb27b82d7298fe74df32b2a5349e00
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-09 14:06:36 -0600

    controller started

---
+++ sim1/controllers/controller.js
@@ -0,0 +1,17 @@
+module.exports = {
+    getBins: (req, res, next) => {
+
+    },
+    getBin: (req, res, next) => {
+
+    },
+    updateBin: (req, res, next) => {
+
+    },
+    delete: (req, res, next) => {
+
+    },
+    newBin: (req, res, next) => {
+        
+    }
+}
--- sim1/server/index.js
+++ sim1/server/index.js
@@ -10,11 +10,11 @@ app.use(bodyParser.json());
 app.use(cors());
 massive (process.env.CONNECTION_STRING).then(dbInstance => app.set ('db', dbInstance));
 
-app.get('/api/shelf/:id');
-app.get('/api/bin/:id');
-app.put('/api/bind:id');
-app.delete('/api/bin/:id');
-app.post('/api/bin/:id');
+app.get('/api/shelf/:id', controller.getBins);
+app.get('/api/bin/:id', controller.getBin);
+app.put('/api/bind:id', controller.updateBin);
+app.delete('/api/bin/:id', controller.delete);
+app.post('/api/bin/:id', controller.newBin);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen (port, () => {console.log(`Server listening on port ${port}.`);});
